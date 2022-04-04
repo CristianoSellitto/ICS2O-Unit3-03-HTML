0b179f15668a1929a341db63ddf3b6306b50ce75
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-04 17:00:11 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit3-03-HTML
+// April 4 2022
 
-'use strict'
 /**
- * Alerts "Hello, World!"
+ * Calculates the volume of a sphere using the radius the user provides in number fields.
  */
-function onButtonClick() {
-  alert("Hello, World!")
+function calculate() {
+  const radius = parseFloat(document.getElementById("radius").value)
+  const answer = ((4 / 3) * Math.PI * (radius ** 3)).toFixed(2)
+
+  document.getElementById("output").innerHTML = "<br><p>The volume of this sphere is " + answer + "cm³</p>"
 }
